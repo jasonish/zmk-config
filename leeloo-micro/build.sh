1ff3ef54b7fd8b9393fd9e0d7a9ef6e7bbc24fd7
Jason Ish <jason@codemonkey.net>
2023-09-29 10:33:06 -0600

    leeloo-micro: enable nice!view

--- leeloo-micro/build.sh
+++ leeloo-micro/build.sh
@@ -5,15 +5,13 @@ set -x
 
 SHIELD="leeloo_micro"
 
-if ! test -e .west; then
-    west init -l /app/config
-    west update
-    west zephyr-export
-fi
+rm -rf .west && west init -l /app/config
+west update
+west zephyr-export
 
 build() {
     side="$1"
-    west build -s zmk/app -d build/${side} -b nice_nano_v2 -- -DZMK_CONFIG=/app/config -DSHIELD=${SHIELD}_${side}
+    west build -s zmk/app -d build/${side} -b nice_nano_v2 -- -DZMK_CONFIG=/app/config -DSHIELD="${SHIELD}_${side} nice_view_adapter nice_view"
     mv ./build/${side}/zephyr/zmk.uf2 ./output/${side}.uf2
 }
 
